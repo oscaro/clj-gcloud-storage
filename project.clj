1aabc8bdaefa8e2c762177426dd0a884beebb1fa
ESTRADA Nicolas <nicolas.estrada@oscaro.com>
2017-11-21 17:58:42 +0100

    Initial commit, ported from Gcloud API, LOTS of untested code

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject com.oscaro/clj-gcloud-storage "0.2.0-SNAPSHOT"
+  :description "Clojure wrapper for google-cloud-storage Java client"
+  :url "https://github.com/oscaro/clj-gcloud-storage"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+                 [com.oscaro/clj-gcloud-common "0.2.0"]
+                 [com.google.cloud/google-cloud-storage "1.12.0"]
+                 [org.clojure/tools.logging "0.4.0"]]
+  :global-vars {*warn-on-reflection* true}
+  :profiles
+  {:dev
+   {:resource-paths ["test-resources"]}})
---
+++ test/com/oscaro/clj_gcloud/storage_test.clj
@@ -0,0 +1,36 @@
+(ns com.oscaro.clj-gcloud.storage-test
+  (:require [clojure.test :refer :all]
+            [clojure.edn :as edn]
+            [com.oscaro.clj-gcloud.storage :refer :all]
+            [clojure.tools.logging :as log])
+  (:import (java.util.concurrent TimeUnit)
+           (com.google.cloud.storage Storage)
+           (com.google.cloud.storage.testing RemoteStorageHelper)))
+
+; Constants
+(def ^String bucket (RemoteStorageHelper/generateBucketName))
+
+(def ^:dynamic ^Storage *storage* nil)
+
+(defn before-tests []
+  (let [info (bucket-info bucket {})]
+    (get-or-create-bucket *storage* info)))
+
+(defn after-tests []
+  (when *storage*
+    (do
+      (log/info "Deleting bucket:" bucket)
+      (when (not (RemoteStorageHelper/forceDelete *storage* bucket 5 TimeUnit/SECONDS))
+        (log/warn "Deletion of bucket" bucket "timed out, bucket is not empty")))))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (binding [*storage* (init (assoc (edn/read-string (slurp "test-resources/test-config.edn"))
+                                     :credentials "test-resources/service-account.json"))]
+      (before-tests)
+      (f)
+      (after-tests))))
+
+(deftest bucket-is-initialized
+  (is (not (nil? (get-bucket *storage* bucket)))))
