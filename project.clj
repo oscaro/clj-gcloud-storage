ac740f9ce0cbb14c8c7d6731f028522d7d4cd67b
ESTRADA Nicolas <nicolas.estrada@oscaro.com>
2017-11-22 12:59:00 +0100

    Using latest common lib

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta4"]
-                 [com.oscaro/clj-gcloud-common "0.2.0"]
+                 [com.oscaro/clj-gcloud-common "0.2.1"]
                  [com.google.cloud/google-cloud-storage "1.12.0"]
                  [org.clojure/tools.logging "0.4.0"]]
   :global-vars {*warn-on-reflection* true}
