d36c29f1948c028109658da1a41ecfefa3e16145
Roland Thiolliere <roland.thiolliere@oscaro.com>
2018-04-13 14:09:47 +0200

    Release 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/clj-gcloud-storage "0.3.0-SNAPSHOT"
+(defproject com.oscaro/clj-gcloud-storage "0.3.0"
   :description "Clojure wrapper for google-cloud-storage Java client"
   :url "http://gitlab.oscaroad.com"
   :license {:name "Eclipse Public License"
