9d6431296e428b29b129defa0bb89e764bbd73f9
ESTRADA Nicolas <nicolas.estrada@oscaro.com>
2018-11-19 11:45:22 +0100

    Preparing for release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.oscaro/clj-gcloud-storage "0.44-1.2"
+(defproject com.oscaro/clj-gcloud-storage "0.44-1.3"
   :description "Clojure wrapper for google-cloud-storage Java client"
   :url "https://github.com/oscaro/clj-gcloud-storage"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.oscaro/clj-gcloud-common "0.44-1.2"]
+                 [com.oscaro/clj-gcloud-common "0.44-1.3"]
                  [com.google.cloud/google-cloud-storage "1.26.0"]
                  [org.clojure/tools.logging "0.4.0"]]
   :global-vars {*warn-on-reflection* true}
