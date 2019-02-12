14543bee1c2a82f11cbf41da3198a21c7b919605
Jeremy Le Corguille <jeremy.lecorguille@oscaro.com>
2019-02-12 15:26:42 +0100

    0.71-1.1 : download-file-from-storage convenience function wrapper

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/clj-gcloud-storage "0.71-1.1-SNAPSHOT"
+(defproject com.oscaro/clj-gcloud-storage "0.71-1.1"
   :description "Clojure wrapper for google-cloud-storage Java client"
   :url "http://gitlab.oscaroad.com"
   :license {:name "Eclipse Public License"
