efb8530d2ffb04aff14551cd984132cb1101ebed
ESTRADA Nicolas <nicolas.estrada@oscaro.com>
2019-04-11 03:30:00 +0200

    Trying out new version

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject com.oscaro/clj-gcloud-storage "0.71-1.3-SNAPSHOT"
+(defproject com.oscaro/clj-gcloud-storage "0.87-1.0-SNAPSHOT"
   :description "Clojure wrapper for google-cloud-storage Java client"
   :url "https://github.com/oscaro/clj-gcloud-storage"
   :license {:name "Eclipse Public License"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.oscaro/clj-gcloud-common "0.71-1.1"]
-                 [com.google.cloud/google-cloud-storage "1.53.0"]
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [com.oscaro/clj-gcloud-common "0.87-1.0-SNAPSHOT"]
+                 [com.google.cloud/google-cloud-storage "1.69.0"]
                  [org.clojure/tools.logging "0.4.0"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [com.google.cloud/google-cloud-nio "0.71.0-alpha"]]}}
+                                  [com.google.cloud/google-cloud-nio "0.87.0-alpha"]]}}
   :repl-options {:init-ns user})
