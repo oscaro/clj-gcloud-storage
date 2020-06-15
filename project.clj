16c5547bf90cfc7bbd26521d83aa44a01a8cb820
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2020-06-15 12:44:53 +0200

    Add deploy-repositories

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "https://github.com/oscaro/clj-gcloud-storage"
   :license {:name "Eclipse Public License"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [com.oscaro/clj-gcloud-common "0.112-1.0"]
                  [com.google.cloud/google-cloud-storage "1.100.0"]
