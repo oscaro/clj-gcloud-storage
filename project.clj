b92586474da2e84f0ce925928540ae1ee9b9b6a4
Mathieu Lirzin <mathieu.lirzin@oscaro.com>
2020-11-22 14:22:55 +0100

    Update org.clojure/* dependencies

--- project.clj
+++ project.clj
@@ -14,9 +14,9 @@
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [com.oscaro/clj-gcloud-common "0.112-1.0"]
                  [com.google.cloud/google-cloud-storage "1.100.0"]
-                 [org.clojure/tools.logging "0.5.0"]]
+                 [org.clojure/tools.logging "1.1.0"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.3.1"]
+                   :dependencies [[org.clojure/tools.namespace "1.0.0"]
                                   [com.google.cloud/google-cloud-nio "0.118.0-alpha"]]}}
   :repl-options {:init-ns user})
