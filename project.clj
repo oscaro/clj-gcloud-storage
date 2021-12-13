58bfacfa783a802220880e154c51d1946c70a472
Ed Porras <dev@digressed.net>
2021-12-13 16:11:50 +0100

    Bump deps based on 0.164.0 (#1)

--- project.clj
+++ project.clj
@@ -12,21 +12,21 @@
   :managed-dependencies
   ;; Google “Bill of Materials” (BOM) defines a combination of
   ;; dependency versions that work well with each other.
-  [[com.google.cloud/google-cloud-bom "0.159.0"
+  [[com.google.cloud/google-cloud-bom "0.164.0"
     :extension "pom"
     :scope "import"]
-   [com.google.cloud/google-cloud-shared-dependencies "2.0.1"
+   [com.google.cloud/google-cloud-shared-dependencies "2.5.1"
     :extension "pom"
     :scope "import"]
    ;; Select non-android Guava to work around Guava versioning mess
    ;; <https://github.com/google/guava/issues/2914>.
    [com.google.guava/guava "30.1.1-jre"]]
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [com.oscaro/clj-gcloud-common "0.159-1.0"]
-                 [com.google.cloud/google-cloud-storage "2.0.1"]
-                 [org.clojure/tools.logging "1.1.0"]]
+                 [com.oscaro/clj-gcloud-common "0.164-1.0"]
+                 [com.google.cloud/google-cloud-storage "2.2.2"]
+                 [org.clojure/tools.logging "1.2.1"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "1.1.0"]
-                                  [com.google.cloud/google-cloud-nio "0.123.6"]]}}
+                   :dependencies [[org.clojure/tools.namespace "1.2.0"]
+                                  [com.google.cloud/google-cloud-nio "0.123.17"]]}}
   :repl-options {:init-ns user})
